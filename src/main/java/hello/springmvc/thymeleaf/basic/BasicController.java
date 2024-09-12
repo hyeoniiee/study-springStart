f1396a0f71237d7be25abc6601bc3cff434329f2
hyeoniiee <starjea0126@gmail.com>
2024-09-12 21:04:58 +0900

    조건부 평가

--- src/main/java/hello/springmvc/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/springmvc/thymeleaf/basic/BasicController.java
@@ -122,4 +122,12 @@ public class BasicController {
         list.add(new User("userC", 30));
         model.addAttribute("users", list);
     }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+
 }
